6a97e43c2f86693027482f37760ffd1ab796c7aa
alomia18 <alomia18@gmail.com>
2023-04-20 18:57:25 -0500

    primer commit

---
+++ api.test.js
@@ -0,0 +1,27 @@
+const request = require('supertest')
+const app = require("./github/workflows/app")
+const sum = require("./sum");
+const { default: expect } = require('expect');
+const { describe } = require('yargs');
+
+// test("sums two numbers", () => {
+//   expect(sum(1, 2)).toBe(3);
+// });
+/**
+ * Testing all user
+ */
+
+  test('testin route to get all objet in the api', (done) => {
+    request(app)
+      .get("/objetos")//metodo a probar
+      .set('Acept', 'application/json')//cabecera de respuesta
+      .expect('Content-Type', /json/) //formato de respuesta
+      .expect(200, done)
+  })
+  test('respond with a json with a single objet', (done) => {
+    request(app)
+      .get("/objetos/1")//metodo a probar
+      .set('Acept', 'application/json')//cabecera de respuesta
+      .expect('Content-Type', /json/) //formato de respuesta
+      .expect(200, done)
+  })
