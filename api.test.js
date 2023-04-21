5ce56427d7253c3c5b5f3bc55d6d0660c9f386ee
alomia18 <alomia18@gmail.com>
2023-04-20 22:12:24 -0500

    commit 6

--- api.test.js
+++ api.test.js
@@ -1,8 +1,8 @@
 const request = require('supertest')
 const app = require("./github/workflows/app")
-const sum = require("./sum");
+//const sum = require("./sum");
 const { default: expect } = require('expect');
-const { describe } = require('yargs');
+//const { describe } = require('yargs');
 
 // test("sums two numbers", () => {
 //   expect(sum(1, 2)).toBe(3);
@@ -11,17 +11,17 @@ const { describe } = require('yargs');
  * Testing all user
  */
 
-  test('testin route to get all objet in the api', (done) => {
-    request(app)
-      .get("/objetos")//metodo a probar
-      .set('Acept', 'application/json')//cabecera de respuesta
-      .expect('Content-Type', /json/) //formato de respuesta
-      .expect(200, done)
-  })
-  test('respond with a json with a single objet', (done) => {
-    request(app)
-      .get("/objetos/1")//metodo a probar
-      .set('Acept', 'application/json')//cabecera de respuesta
-      .expect('Content-Type', /json/) //formato de respuesta
-      .expect(200, done)
-  })
+test('testin route to get all objet in the api', (done) => {
+  request(app)
+    .get("/objetos")//metodo a probar
+    .set('Acept', 'application/json')//cabecera de respuesta
+    .expect('Content-Type', /json/) //formato de respuesta
+    .expect(200, done)
+})
+test('respond with a json with a single objet', (done) => {
+  request(app)
+    .get("/objetos/1")//metodo a probar
+    .set('Acept', 'application/json')//cabecera de respuesta
+    .expect('Content-Type', /json/) //formato de respuesta
+    .expect(200, done)
+})
